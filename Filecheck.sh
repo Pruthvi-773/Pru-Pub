7cd7e955d63d3ab11106cd7467116c74929bd6f6
pruthvi.allepelli@gmail.com <pruthvi.allepelli@gmail.com>
2023-03-01 16:18:26 +0530

    Added File check Script

---
+++ Filecheck.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Going to create file using shell script"
+read filename
+
+if [ -f $filename ]; 
+	then
+	 echo "File $filename Already Exist"
+
+else touch $filename 
+		 echo " Creating  $filename since it is not Exist in the $PWD "
+
+fi
